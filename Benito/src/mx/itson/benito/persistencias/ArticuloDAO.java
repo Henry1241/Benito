651b7c19a780d0be3a22295d5d5828bd984c07d8
Henry1241 <enri09ley@outlook.com>
2023-03-15 19:22:06 -0700

    Persistencias
    Datos y parámetros añadidos en la clase ArticuloDAO yProveedorDAO

--- Benito/src/mx/itson/benito/persistencias/ArticuloDAO.java
+++ Benito/src/mx/itson/benito/persistencias/ArticuloDAO.java
@@ -5,10 +5,141 @@
  */
 package mx.itson.benito.persistencias;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import mx.itson.benito.entidades.Articulo;
+import mx.itson.benito.entidades.Proveedor;
+import mx.itson.benito.utilerias.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
 /**
  *
  * @author enri0
  */
 public class ArticuloDAO {
     
+    /**
+     * Conecta con Hibernate y con la base de datos MySQL para obtener datos de
+     * la base de datos especifica.
+     * @return multas
+     */
+    public static List<Articulo> obtenerTodos(){
+        List<Articulo> articulos = new ArrayList<>();
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            CriteriaQuery<Articulo> criteriaQuery = 
+                    session.getCriteriaBuilder().createQuery(Articulo.class);
+            criteriaQuery.from(Articulo.class);
+            
+            articulos = session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception ex) {
+            System.err.println("Ocurrio un error: " + ex.getMessage());
+        }
+        return articulos;
+    }
+    /**
+     * Metodo encargado de registrar los datos que seran usados para guardar
+     * una multa nueva.
+     * @param clave
+     * @param nombre
+     * @param precio
+     * @param Proveedor
+     * @return resultado
+     */
+    public static boolean guardar(String clave, String nombre, double precio, Proveedor Proveedor){
+        boolean resultado = false;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            
+            Articulo a = new Articulo();
+            a.setClave(clave);
+            a.setNombre(nombre);
+            a.setPrecio(precio);
+            a.setIdProveedor(Proveedor);
+            
+            session.save(a);
+            
+            session.getTransaction().commit();
+            
+            resultado = a.getId() != 0;
+        } catch (Exception ex) {
+            System.err.println("Ocurrio un error" + ex.getMessage());
+        }
+        return resultado;
+    }
+   /**
+    * Metodo que se encarga de buscar un parametro a traves de su id.
+    * @param id
+    * @return multa
+    */
+    public static Articulo obtenerPorId(int id){
+        Articulo articulo = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            articulo = session.get(Articulo.class, id);
+        } catch (HibernateException ex) {
+            System.err.println("Ocurrio un error: " + ex.getMessage());
+        }
+        return articulo;
+    }
+    /**
+     * Metodo que se encarga de seleccionar los parametros a editar.
+     * @param id
+     * @param clave
+     * @param nombre
+     * @param precio
+     * @param proveedor
+     * @return resultado
+     */
+    public static boolean editar(int id, String clave, String nombre, double precio, Proveedor proveedor){
+        boolean resultado = false;
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            
+            Articulo articulo = obtenerPorId(id);
+            if(articulo != null){
+                articulo.setClave(clave);
+                articulo.setNombre(nombre);
+                articulo.setPrecio(precio);
+                articulo.setIdProveedor(proveedor);
+                
+                session.saveOrUpdate(articulo);              
+                session.getTransaction().commit();
+                resultado = true;
+            }
+        } catch (HibernateException ex) {
+            System.err.println("Ocurrio un error: " + ex.getMessage());
+        }
+        return resultado;
+    }
+    /**
+     * Metodo que se encarga de eliminar usando el id como referencia.
+     * @param id
+     * @return resultado
+     */
+    public static boolean eliminar(int id){
+        boolean resultado = false;
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            
+            Articulo articulo = obtenerPorId(id);
+            articulo.getIdProveedor().getId();
+            
+            if(articulo != null){
+                session.delete(articulo);
+                HibernateUtil.getSessionFactory().getCurrentSession().delete(articulo);
+                session.getTransaction().commit();
+                resultado = true;
+            }
+        } catch (HibernateException ex) {
+            System.err.println("Ocurrio un error: " + ex.getMessage());
+        }
+        return resultado;
+    }
 }
--- Benito/src/mx/itson/benito/persistencias/ProveedorDAO.java
+++ Benito/src/mx/itson/benito/persistencias/ProveedorDAO.java
@@ -5,10 +5,152 @@
  */
 package mx.itson.benito.persistencias;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import mx.itson.benito.entidades.Articulo;
+import mx.itson.benito.entidades.Proveedor;
+import mx.itson.benito.utilerias.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
 /**
  *
  * @author enri0
  */
 public class ProveedorDAO {
     
+    /**
+     * Conecta con Hibernate y con la base de datos MySQL para obtener datos de
+     * la base de datos especifica.
+     * @return multas
+     */
+    public static List<Proveedor> obtenerTodos(){
+        List<Proveedor> proveedores = new ArrayList<>();
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            CriteriaQuery<Proveedor> criteriaQuery = 
+                    session.getCriteriaBuilder().createQuery(Proveedor.class);
+            criteriaQuery.from(Articulo.class);
+            
+            proveedores = session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception ex) {
+            System.err.println("Ocurrio un error: " + ex.getMessage());
+        }
+        return proveedores;
+    }
+    /**
+     * Metodo encargado de registrar los datos que seran usados para guardar
+     * una multa nueva.
+     * @param clave
+     * @param nombre
+     * @param direccion
+     * @param telefono
+     * @param email
+     * @param contacto
+     * @param articulos
+     * @return resultado
+     */
+    public static boolean guardar(String clave, String nombre, String direccion, String telefono, String email, String contacto, List<Articulo> articulos){
+        boolean resultado = false;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            
+            Proveedor p = new Proveedor();
+            p.setClave(clave);
+            p.setNombre(nombre);
+            p.setDireccion(direccion);
+            p.setTelefono(telefono);
+            p.setEmail(email);
+            p.setContacto(contacto);
+            p.setArticulos(articulos);
+            
+            session.save(p);
+            
+            session.getTransaction().commit();
+            
+            resultado = p.getId() != 0;
+        } catch (Exception ex) {
+            System.err.println("Ocurrio un error" + ex.getMessage());
+        }
+        return resultado;
+    }
+   /**
+    * Metodo que se encarga de buscar un parametro a traves de su id.
+    * @param id
+    * @return proveedor
+    */
+    public static Proveedor obtenerPorId(int id){
+        Proveedor proveedor = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            proveedor = session.get(Proveedor.class, id);
+        } catch (HibernateException ex) {
+            System.err.println("Ocurrio un error: " + ex.getMessage());
+        }
+        return proveedor;
+    }
+    /**
+     * Metodo que se encarga de seleccionar los parametros a editar.
+     * @param id
+     * @param clave
+     * @param nombre
+     * @param direccion
+     * @param telefono
+     * @param email
+     * @param contacto
+     * @param articulos
+     * @return resultado
+     */
+    public static boolean editar(int id, String clave, String nombre, String direccion, String telefono, String email, String contacto, List<Articulo> articulos){
+        boolean resultado = false;
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            
+            Proveedor proveedor = obtenerPorId(id);
+            if(proveedor != null){
+                proveedor.setClave(clave);
+                proveedor.setNombre(nombre);
+                proveedor.setDireccion(direccion);
+                proveedor.setTelefono(telefono);
+                proveedor.setEmail(email);
+                proveedor.setContacto(contacto);
+                proveedor.setArticulos(articulos);
+                
+                session.saveOrUpdate(proveedor);              
+                session.getTransaction().commit();
+                resultado = true;
+            }
+        } catch (HibernateException ex) {
+            System.err.println("Ocurrio un error: " + ex.getMessage());
+        }
+        return resultado;
+    }
+    /**
+     * Metodo que se encarga de eliminar usando el id como referencia.
+     * @param id
+     * @return resultado
+     */
+    public static boolean eliminar(int id){
+        boolean resultado = false;
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            
+            Proveedor proveedor = obtenerPorId(id);
+            proveedor.getArticulos().get(id);
+            
+            if(proveedor != null){
+                session.delete(proveedor);
+                HibernateUtil.getSessionFactory().getCurrentSession().delete(proveedor);
+                session.getTransaction().commit();
+                resultado = true;
+            }
+        } catch (HibernateException ex) {
+            System.err.println("Ocurrio un error: " + ex.getMessage());
+        }
+        return resultado;
+    }
 }
